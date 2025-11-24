c35330e6de6f714f0886955bfc670e52cf1a8b42
Ce1ikE <enniscelik@gmail.com>
2025-11-24 08:36:50 +0100

    bug fixes

--- Lecture_8_Tensorboard/main.py
+++ Lecture_8_Tensorboard/main.py
@@ -9,7 +9,7 @@ def main():
     ROOT = Path(__file__).parent
 
     # rnn_languages_pytorch(ROOT)
-    custom_languages_lstm_pytorch(ROOT)
+    # custom_languages_lstm_pytorch(ROOT)
     # cnn_fashion_mnist_pytorch(ROOT)
     # cnn_beexants_pytorch(ROOT)
 
